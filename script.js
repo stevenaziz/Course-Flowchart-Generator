85de537ad5ddcad14cb87cf4e5d6e040665668b0
Steven Aziz <hello@stevenaziz.com>
2023-03-17 01:18:40 -0700

    Fixed syntax errors in index.html and implemented parsing function

--- script.js
+++ script.js
@@ -20,5 +20,71 @@ function processInput(e) {
 }
 
 function parseFile() {
-    console.log(fileInput.result);
+    let coursesArr = fileInput.result.split(/\r?\n/); // split by new line
+    let courseCodeNameCredsArr = [];
+    let coursePrereqsAvailArr = [];
+    let coursePrereqsArr = [];
+    let courseAvailArr = [];
+    let courseObjArr = [];
+    let currObj = {};
+    let availAutumnArr = [];
+    let availWinterArr = [];
+    let availSpringArr = [];
+    let noPrereqsArr = [];
+
+    coursesArr.forEach(courseInfo => {
+        courseCodeNameCredsArr = courseInfo.split(",", 3);
+        
+        coursePrereqsAvailArr = courseInfo.split("[");
+        coursePrereqsAvailArr.shift(); // Removes course code, name, and credits (already processed)
+
+        coursePrereqsAvailArr[0] = coursePrereqsAvailArr[0].trim();
+        coursePrereqsArr = coursePrereqsAvailArr[0].split(",");
+        coursePrereqsArr.pop();
+        coursePrereqsArr.forEach((element, index, array) => {
+            if (+(element.replace(/\D/g, "")) != 0) {
+                array[index] = +(element.replace(/\D/g, ""));
+            } else {
+                array.shift();
+            }
+        });
+
+        courseAvailArr = coursePrereqsAvailArr[1].split(",");
+        courseAvailArr.forEach((element, index, array) => {
+            array[index] = +(element.replace(/\D/g, ""));
+        });
+
+        currObj = {
+            code: courseCodeNameCredsArr[0],
+            id: +(courseCodeNameCredsArr[0].replace(/\D/g, "")), // replace anything NOT a digit with ""
+            name: courseCodeNameCredsArr[1].trim(),
+            credits: parseInt(courseCodeNameCredsArr[2]),
+            prereqs: coursePrereqsArr,
+            avail: courseAvailArr
+        };
+
+        courseObjArr.push(currObj);
+
+        courseAvailArr.forEach(element => {
+            if (element == 1) {
+                availAutumnArr.push(currObj);
+            }
+            if (element == 2) {
+                availWinterArr.push(currObj);
+            }
+            if (element == 3) {
+                availSpringArr.push(currObj);
+            }
+        });
+
+        if (coursePrereqsArr.length == 0) {
+            noPrereqsArr.push(currObj);
+        }
+    });
+
+    console.log(courseObjArr);
+    console.log(availAutumnArr);
+    console.log(availWinterArr);
+    console.log(availSpringArr);
+    console.log(noPrereqsArr);
 }
