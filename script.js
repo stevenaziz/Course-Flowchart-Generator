60d7217e911fbe808854d00302819f5c9cf2e7b6
Steven Aziz <hello@stevenaziz.com>
2023-03-17 20:35:37 -0700

    Added Toposort library

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// Variables
+// Global Variables
 const fileInput = new FileReader();
-let courseObjArr = []; // all courses
+let maxCredits;
+let startQtr;
+let courseObjArr = [];
+let edgesArr = [];
+let nodesArr = [];
 let availAutumnArr = [];
 let availWinterArr = [];
 let availSpringArr = [];
@@ -14,12 +18,19 @@ const submitBtn = document.querySelector("#submit-btn");
 // Listeners
 submitBtn.addEventListener("click", processInput);
 
-// Upon submit button, capture form data, read file, and call parsing function
+// Capture form data, read file, and call parsing function
 function processInput(e) {
     e.preventDefault();
 
-    const maxCredits = inputMaxCredits.value;
-    const startQtr = document.querySelector('input[name="start-qtr"]:checked').value;
+    courseObjArr = [];
+    edgesArr = [];
+    nodesArr = [];
+    availAutumnArr = [];
+    availWinterArr = [];
+    availSpringArr = [];
+    noPrereqsArr = [];
+    maxCredits = inputMaxCredits.value;
+    startQtr = document.querySelector('input[name="start-qtr"]:checked').value;
 
     fileInput.readAsText(inputFile.files[0]);
     fileInput.addEventListener("load", parseFile);
@@ -33,19 +44,26 @@ function parseFile() {
     let coursePrereqsArr = [];
     let courseAvailArr = [];
     let currObj = {};
+    let currID;
+    let tempID;
 
     coursesArr.forEach(courseInfo => {
         courseCodeNameCredsArr = courseInfo.split(",", 3);
+
+        currID = +(courseCodeNameCredsArr[0].replace(/\D/g, "")), // replace anything NOT a digit with empty string
+        nodesArr.push(currID);
         
         coursePrereqsAvailArr = courseInfo.split("[");
         coursePrereqsAvailArr.shift(); // Removes course code, name, and credits (already processed)
 
-        coursePrereqsAvailArr[0] = coursePrereqsAvailArr[0].trim();
+        coursePrereqsAvailArr[0] = coursePrereqsAvailArr[0].trim(); // remove start-end whitespace
         coursePrereqsArr = coursePrereqsAvailArr[0].split(",");
-        coursePrereqsArr.pop();
+        coursePrereqsArr.pop(); // remove last element - empty
         coursePrereqsArr.forEach((element, index, array) => {
-            if (+(element.replace(/\D/g, "")) != 0) {
+            tempID = +(element.replace(/\D/g, ""));
+            if (tempID != 0) {
                 array[index] = +(element.replace(/\D/g, ""));
+                edgesArr.push([currID, tempID]);
             } else {
                 array.shift();
             }
@@ -58,14 +76,17 @@ function parseFile() {
 
         currObj = {
             code: courseCodeNameCredsArr[0],
-            id: +(courseCodeNameCredsArr[0].replace(/\D/g, "")), // replace anything NOT a digit with ""
+            id: currID,
             name: courseCodeNameCredsArr[1].trim(),
             credits: parseInt(courseCodeNameCredsArr[2]),
             prereqs: coursePrereqsArr,
-            avail: courseAvailArr
+            avail: courseAvailArr,
+            group: 0
         };
 
         courseObjArr.push(currObj);
+
+        // console.log(`{code: ${currObj.code}, id: ${currObj.id}, name: ${currObj.name}, credits: ${currObj.credits}, prereqs: [${currObj.prereqs}], avail: [${currObj.avail}]}, `);
 
         courseAvailArr.forEach(element => {
             if (element == 1) {
@@ -83,10 +104,12 @@ function parseFile() {
             noPrereqsArr.push(currObj);
         }
     });
+    
+    console.log(toposort(nodesArr, edgesArr).reverse());
+}
 
-    console.log(courseObjArr);
-    console.log(availAutumnArr);
-    console.log(availWinterArr);
-    console.log(availSpringArr);
-    console.log(noPrereqsArr);
+function groupCoursesByQtr(start_qtr, max_creds) {
+    let currQtr = start_qtr;
+
+    return toposort(nodesArr, edgesArr).reverse();
 }
